42e148bb50b9addf386ca1bbb4340cbbcf0d2380
denysrichter4 <denysrichter4@gmail.com>
2021-10-21 21:43:34 -0300

    push inicial

---
+++ main.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask import render_template
+
+
+app = Flask(__name__)
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)
+
+@app.route('/projects/')
+def projects():
+    return 'The project page'
+
+@app.route('/about')
+def about():
+    return 'The about page'
