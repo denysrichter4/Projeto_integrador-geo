9e424701f791b50e77b41fbbbbb14a65b40cb664
denysrichter4 <denysrichter4@gmail.com>
2021-10-24 00:11:09 -0300

    desenvolvida pagina de login

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from flask import Flask
 from flask import render_template
+from flask import url_for
 
 
 app = Flask(__name__)
 
-@app.route('/index/')
+@app.route('/')
 def index():
     return render_template('index.html')
 
