40d014e4d4f7db71cf89238763f57517dbd3dcaf
denysrichter4 <denysrichter4@gmail.com>
2021-10-24 17:18:47 -0300

    refatorando arquiteruta para MVC

--- app/main.py
+++ app/main.py
@@ -2,12 +2,15 @@ from flask import Flask
 from flask import render_template
 from flask import url_for
 
+# ---> comando para iniciar o ambiente ---> . venv/bin/activate 
+# ---> comando para iniciar o projeto ---> export FLASK_APP=main 
+# ---> comando para buildar o projeto ---> flask run  
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('login.html')
 
 @app.route('/login/')
 def login():
