b66265a9d5d0c4a02d8254351bfabdfc8f99d5ff
denysrichter4 <denysrichter4@gmail.com>
2021-10-24 21:32:26 -0300

    desenvolvida uma parte do backend e boa parte do frontend

--- run.py
+++ run.py
@@ -1,8 +1,10 @@
-from app import manager
+from app import app
 
-# ---> comando para iniciar o ambiente ---> . venv/bin/activate 
-# ---> comando para iniciar o projeto ---> export FLASK_APP=run 
+# ---> comando para iniciar o ambiente ---> . venv/bin/activate / venv\Scripts\activate
+# ---> comando para iniciar o projeto ---> export FLASK_APP=run / set FLASK_APP=run
 # ---> comando para buildar o projeto ---> flask run  
 
+#if __name__=="__main__":
+    #manager.run()
 if __name__=="__main__":
-    manager.run()
+    app.run()
